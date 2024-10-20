6dca4e63b0d454156bbd25fdf07162059cb217c8
unknown <alexandreliberatto@gmail.com>
2024-10-20 13:35:58 -0300

    fazendo update
    Signed-off-by: unknown <alexandreliberatto@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [MailModule],
