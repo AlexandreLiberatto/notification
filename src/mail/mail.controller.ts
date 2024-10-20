cd874d159e3d8b9fec5234192a6dbeefd483dec6
unknown <alexandreliberatto@gmail.com>
2024-10-20 15:43:07 -0300

    Implementando métodows para ler os e-mails
    Signed-off-by: unknown <alexandreliberatto@gmail.com>

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,7 +1,66 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { Mail } from '@prisma/client';
+import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
+import { DataMessage } from './types/message';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
+
+  private readonly logger = new Logger(MailController.name);
+
+  @Get('get')
+  async getMail(@Query('idUser') idUser: string): Promise<Mail[] | null> {
+    return await this.mailService.getMailByIdUser(idUser);
+  }
+
+  @MessagePattern('register')
+  async readRegisterPayment(
+    @Payload() payload: any,
+    @Ctx() context: RmqContext,
+  ) {
+
+    try {
+
+      this.logger.log(`data: ${JSON.stringify(payload)}`);
+      const DataMessage: DataMessage = JSON.parse(payload.data.notification);
+      const channel = context.getChannelRef();
+      const originalMesage = context.getMessage();
+      channel.ack(originalMesage);
+      await this.mailService.sendMail(DataMessage, MailType.orderConfirmation);
+      await this.mailService.persistNotification(
+        DataMessage,
+        MailType.orderConfirmation,
+    );
+      
+    } catch (error) {
+      
+    }
+    
+  }
+
+  @MessagePattern('confirmation')
+  async readConfirmationPayment(
+    @Payload() payload: any,
+    @Ctx() context: RmqContext,
+  ) {
+    try {
+
+      const DataMessage: DataMessage = JSON.parse(payload.data.notification);
+      const channel = context.getChannelRef();
+      const originalMesage = context.getMessage();
+      channel.ack(originalMesage);
+      await this.mailService.sendMail(
+        DataMessage,
+        MailType.paymentConfirmation,
+      );
+      await this.mailService.persistNotification(
+        DataMessage,
+        MailType.paymentConfirmation,
+    );
+      
+    } catch (error) {   
+    }
+  }
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,43 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { DataMessage } from './types/message';
+import { Mail } from '@prisma/client';
+
 
 @Injectable()
 export class MailService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async getMailByIdUser(idUser: string): Promise<Mail[] | null> {
-        return await this.prisma.mail.fyndMany({
-            where: { idUser },
-        });
+  async getMailByIdUser(idUser: string): Promise<Mail[] | null> {
+    return await this.prisma.mail.findMany({
+      where: { idUser },
+    });
+  }
+
+  async sendMail(content: DataMessage, type: MailType) {
+    console.log(`sendMail - content = ${content} - type = ${type}`);
+    // Implementar método para conectar e enviar e-mail...
+  }
+
+  async persistNotification(content: DataMessage, type: MailType) {
+    const data = {
+      idUser: content.idUser,
+      mailDestination: this.getDestination(content.idUser),
+      mailContent: this.makeContent(content.orderNumber, content.orderValue),
+      mailType: type,
+    };
+
+    await this.prisma.mail.create({ data });
+  }
+
+  getDestination(idUser: string) {
+    switch (idUser) {
+      case '10':
+        return 'user@teste.com.br';
+      default:
+        return 'default@teste.com.br';
     }
+  }
 
-    async sendMail(content: DataMessage, type: MailType) {
-        console.log(`sendMail - content = ${content} - type = ${type}`);
-        //implementar método para conectar e-mail...
-    }
-
-    async persistNotification(content: DataMessage, type: MailType) {
-        const data = {
-            idUser: content.idUser,
-            mailDestination: this.getDestination(content.idUser),
-            mailContent: this.makeContent(content.orderNumber, content.orderValue),
-            mailType: type,
-        };
-
-        await this.prisma.mail.create({ data: { ...data} });
-    }
-
-    getDestination(idUser: string){
-        switch (idUser){
-            case '10':
-                return 'user@teste.com.br'
-            default:
-                return 'default@teste.com.br'
-        }
-    }
-
-    makeContent(orderNumber:number, orderValue: number){
-        return `Número do pedido: ${orderNumber} - Valor do pedido: ${orderValue}`;
-    }
+  makeContent(orderNumber: number, orderValue: number) {
+    return `Número do pedido: ${orderNumber} - Valor do pedido: ${orderValue}`;
+  }
 }
