bcf77cf168f8da1bfae8a48d85e48c353a6b5624
unknown <alexandreliberatto@gmail.com>
2024-10-20 14:10:46 -0300

    Criando método para consulta na tabela Mail
    Signed-off-by: unknown <alexandreliberatto@gmail.com>

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+    constructor(private prisma: PrismaService) {}
+
+    async getMailByIdUser(idUser: string): Promise<Mail[] | null> {
+        return await this.prisma.mail.fyndMany({
+            where: { idUser },
+        });
+    }
+}
